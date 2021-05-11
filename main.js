0d73e3115a1e01fc34c16c296d5a9cdf60c46aea
AnatoliyDemiurge <sharamradov@gmail.com>
2021-05-11 15:44:40 +0300

    END VALIDATION Beta

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const str = document.querySelector('.field');
 const btn = document.querySelector('.btn');
 str.addEventListener('input',()=>{
     var strValue = str.value;
+    console.log(typeof(strValue));
     var strValueClean = strValue.split('.');
     const regexp = /[^0-9 | ^\.]/mg;
     if ((typeof(strValue)) == null || strValue == '' || strValue.match(regexp) || (strValueClean.length > 2)){
@@ -10,9 +11,23 @@ str.addEventListener('input',()=>{
     }else{
         let strOutput = strValue,
             maxNumber = 201.23;
+        var strValueCleanFractionPart = strValueClean[1];
+        var strValueCleanUnitPart = strValueClean[0];
+        var strNulsCleanUnitPart = strValueCleanUnitPart.split('');
+        if (strNulsCleanUnitPart[0] == 0) {
+            setTimeout(function(){
+                let newStr = strNulsCleanUnitPart.join('');
+                let newResult = parseInt(newStr,10);
+                strValueCleanUnitPart = newResult;
+                if (strValueClean.length > 1){
+                    strOutput = strValueCleanUnitPart + '.' + strValueCleanFractionPart;
+                }else{
+                    strOutput = strValueCleanUnitPart;
+                }
+                str.value = strOutput;
+            },100)
+        }
         if (strValueClean.length > 1) {
-            var strValueCleanFractionPart = strValueClean[1];
-            var strValueCleanUnitPart = strValueClean[0];
             if (strValueCleanFractionPart.length > 2) {
                 setTimeout(function(){ 
                     let arr = strValueCleanFractionPart.split('');
@@ -26,8 +41,7 @@ str.addEventListener('input',()=>{
         }
         if (str.value > maxNumber) {
             str.value = maxNumber;
+            
         }
     }
 });
-
-
